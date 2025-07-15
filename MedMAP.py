065cdff2919494fe68a483b7fcae97e25ec9bb4e
Ty <95022615+T-Y-Liu@users.noreply.github.com>
2025-07-15 02:10:54 -0700

    Add files via upload

--- MedMAP.py
+++ MedMAP.py
@@ -1,90 +1,51 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-
-class ConvBlock(nn.Module):
-    def __init__(self, n_stages, n_filters_in, n_filters_out, normalization='none'):
-        super(ConvBlock, self).__init__()
-
-        ops = []
-        for i in range(n_stages):
-            if i==0:
-                input_channel = n_filters_in
-            else:
-                input_channel = n_filters_out
-
-            ops.append(nn.Conv3d(input_channel, n_filters_out, 3, padding=1,bias=False))
-            if normalization == 'batchnorm':
-                ops.append(nn.BatchNorm3d(n_filters_out))
-            elif normalization == 'groupnorm':
-                ops.append(nn.GroupNorm(num_groups=16, num_channels=n_filters_out))
-            elif normalization == 'instancenorm':
-                ops.append(nn.InstanceNorm3d(n_filters_out))
-            elif normalization != 'none':
-                assert False
-            ops.append(nn.ReLU(inplace=True))
-
-        self.conv = nn.Sequential(*ops)
-
-    def forward(self, x):
-        x = self.conv(x)
-        return x
-
-class Model(nn.Module):
-    def __init__(self, num_cls=4):
-        super(Model, self).__init__()
-
-        self.embed = ConvBlock(1, 4, 4, normalization='batchnorm')
-        self.latent = ConvBlock(1, 4, 4, normalization='batchnorm')
-
-        self.weights = nn.Parameter(torch.empty(4))  
-        nn.init.normal_(self.weights, mean=0.0, std=0.1)  
-        
-    def align_pstarmix(self, input):
-        input = self.embed(input)
-        input = self.latent(input)
-        t1 = input[:,1,:].unsqueeze(1)
-        t2 = input[:,3,:].unsqueeze(1)
-        t1ce = input[:,2,:].unsqueeze(1)
-        flair = input[:,0,:].unsqueeze(1)
-
-        w = F.softmax(self.weights, dim=0)
-        w_sum = self.weights.sum()
-        w = self.weights / w_sum
-        
-        p_mix = w[0] * t1.detach() + w[1] * t2.detach() + \
-                w[2] * t1ce.detach() + w[3] * flair.detach()
-
-        align_loss = F.mse_loss(t1, p_mix) + \
-                     F.mse_loss(t2, p_mix) + \
-                     F.mse_loss(t1ce, p_mix) + \
-                     F.mse_loss(flair, p_mix)
-        
-
-        input = torch.cat((flair, t1, t1ce, t2), 1) 
-        return input, align_loss/4
-
-    def align_pNmix(self, input):
-        align_loss = 0
-        for f in range(input.size()[1]):
-            # print(input[:,f,:].size())
-            mean = self.mean_enc
-            # log_var = self.log_var_enc
-            var = self.log_var_enc
-            
-            vlb = (mean - input[:,f,:]).pow(2).div(var) + var
-            align_loss += vlb.mean() / 2
-        return input, align_loss/4
-
-    
-    def forward(self, x, mask):
-        # x, medmap_loss = self.align_pstarmix(x)
-        x, medmap_loss = self.align_pNmix(x)
-        
-        ########ORIGINAL MODEL#######
-        # x -> fuse_pred
-
         if self.is_training:
-            return fuse_pred, medmap_loss
-        return fuse_pred
+            # softmax weight
+            w = F.softmax(self.weights, dim=0)
+
+            # 直接用原始特征，不 detach
+            p_mix = w[2] * t1_x1 + w[3] * t2_x1 + w[1] * t1ce_x1 + w[0] * flair_x1
+
+            # alignment loss
+            align_loss = (F.mse_loss(t1_x1, p_mix) +
+                        F.mse_loss(t2_x1, p_mix) +
+                        F.mse_loss(t1ce_x1, p_mix) +
+                        F.mse_loss(flair_x1, p_mix)) / 8
+
+            # 替换缺失模态
+            final_features = []
+            modal_features = [flair_x1, t1ce_x1, t1_x1, t2_x1]
+            for i in range(4):
+                if mask[0, i].item():
+                    final_features.append(modal_features[i])  # 不需要乘w[i]
+                else:
+                    final_features.append(p_mix)  # 可以乘w[i]也可以不乘，看设计
+           
+            flair_x1, t1ce_x1, t1_x1, t2_x1 = final_features
+
+        else:
+            align_loss = torch.tensor(0.0, device=x.device)
+
+            w = F.softmax(self.weights, dim=0)
+
+            modal_features = [flair_x1, t1ce_x1, t1_x1, t2_x1]
+            available_features = []
+            available_weights = []
+            for i in range(4):
+                if mask[0, i].item():
+                    available_features.append(modal_features[i])
+                    available_weights.append(w[i])
+
+
+            weights = torch.stack(available_weights)
+            weights = weights / weights.sum()  
+            p_mix = sum(wi * fi for wi, fi in zip(weights, available_features))
+
+            # 构建最终的四个特征
+            final_features = []
+            for i in range(4):
+                if mask[0, i].item():
+                    final_features.append(w[i] * modal_features[i])  
+                else:
+                    final_features.append(w[i] * p_mix)              
+
+            flair_x1, t1ce_x1, t1_x1, t2_x1 = final_features
